dc6490aa6946ef687280da85d09bf8cb8f3f44a4
Rob Morrison <mrrsn@live.com>
2018-02-04 16:00:52 -0500

    Move melodies to a header file

--- light-it-up.ino
+++ light-it-up.ino
@@ -7,20 +7,10 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_Circuit_Playground.h>
 #include "pitches.h"
+#include "melodies.h"
 uint32_t _offColor = 0x0000FF;
 uint32_t _onColor = 0x00FF00;
 const int _brightness = 1;
-const int _helloMelody[] = { NOTE_C3, NOTE_C3, NOTE_G3, NOTE_C3, 0, NOTE_C4, NOTE_C4, NOTE_B3, NOTE_C4 };
-const int _helloMelodyLen = 9;
-const int _winMelody[] = { NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, 0, 0, 0, 0, 0, NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5 };
-const int _winMelodyLen = 13;
-const int _levelUpMelody[] = { NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, 0, 0, 0, 0, 0, NOTE_C5, NOTE_G4, NOTE_E4, NOTE_C4 };
-const int _levelUpMelodyLen = 13;
-const int _oopsMelody[] = { NOTE_C4, NOTE_F3 };
-const int _oopsMelodyLen = 2;
-const int _startTune[] = { NOTE_C4, NOTE_C4, NOTE_C5 };
-const int _offTune[] = { NOTE_C2, NOTE_G2, NOTE_D2, NOTE_G2, NOTE_E2, NOTE_G2, NOTE_F2, NOTE_G2, NOTE_E2, NOTE_G2 };
-const int _onTune[] = { NOTE_C3, NOTE_G3, NOTE_D3, NOTE_G3, NOTE_E3, NOTE_G3, NOTE_F3, NOTE_G3, NOTE_E3, NOTE_G3 };
 const int _LEDs = 10;
 const int _levels = 10;
 int _currSkill = 0;
---
+++ melodies.h
@@ -0,0 +1,16 @@
+const int _helloMelody[] = { NOTE_C3, NOTE_C3, NOTE_G3, NOTE_C3, 0, NOTE_C4, NOTE_C4, NOTE_B3, NOTE_C4 };
+const int _helloMelodyLen = 9;
+
+const int _winMelody[] = { NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, 0, 0, 0, 0, 0, NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5 };
+const int _winMelodyLen = 13;
+
+const int _levelUpMelody[] = { NOTE_C4, NOTE_E4, NOTE_G4, NOTE_C5, 0, 0, 0, 0, 0, NOTE_C5, NOTE_G4, NOTE_E4, NOTE_C4 };
+const int _levelUpMelodyLen = 13;
+
+const int _oopsMelody[] = { NOTE_C4, NOTE_F3 };
+const int _oopsMelodyLen = 2;
+
+const int _startTune[] = { NOTE_C4, NOTE_C4, NOTE_C5 };
+const int _offTune[] = { NOTE_C2, NOTE_G2, NOTE_D2, NOTE_G2, NOTE_E2, NOTE_G2, NOTE_F2, NOTE_G2, NOTE_E2, NOTE_G2 };
+const int _onTune[] = { NOTE_C3, NOTE_G3, NOTE_D3, NOTE_G3, NOTE_E3, NOTE_G3, NOTE_F3, NOTE_G3, NOTE_E3, NOTE_G3 }
+
