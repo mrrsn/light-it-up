3b712840c2f53288e2f2483183a61f6395b35f71
Rob Morrison <mrrsn@live.com>
2018-02-13 21:37:38 -0500

    Modify and add colors to palette

--- lights.h
+++ lights.h
@@ -1,22 +1,23 @@
 #ifndef LIGHTS_H
 #define LIGHTS_H
 
-struct COLOR { uint8_t red; uint8_t green; uint8_t blue; };
+struct COLOR { uint8_t red, green, blue; };
 
-const COLOR red = { red: 255, green: 0, blue: 0 };
-const COLOR orange = { red: 255, green: 128, blue: 0 };
-const COLOR yellow = { red: 255, green: 255, blue: 0 };
-const COLOR green = { red: 0, green: 255, blue: 0 };
-const COLOR blue = { red: 0, green: 0, blue: 255 };
-const COLOR lightBlue = { red: 119, green: 255, blue: 255 };
-const COLOR lightRed = { red: 255, green: 52, blue: 86 };
-const COLOR electricBlue = { red: 125, green: 249, blue: 255 };
-const COLOR bob = { red: 176, green: 191, blue: 255 };
+const COLOR red = { 255, 0, 0 };
+const COLOR orange = { 255, 128, 0 };
+const COLOR yellow = { 255, 255, 0 };
+const COLOR green = { 0, 255, 0 };
+const COLOR blue = { 0, 0, 255 };
+const COLOR lightBlue = { 119, 255, 255 };
+const COLOR lightRed = { 255, 52, 86 };
+const COLOR electricBlue = { 125, 249, 255 };
+const COLOR bob = { 176, 177, 17 }; // 0xB0B111 :D
+const COLOR dave = { 218, 94, 17 }; // 0xDA5E11 :D
 
 const COLOR _readySetGo[] = { red, yellow, green };
 
-const COLOR _nicePalette[] = { red, blue, lightBlue, lightRed, electricBlue, orange, bob };
-const short _nicePaletteCount = 7;
+const COLOR _nicePalette[] = { red, blue, lightBlue, lightRed, electricBlue, orange, bob, dave };
+const short _nicePaletteCount = 8;
 
 COLOR _offColor = blue;
 COLOR _onColor = green;
