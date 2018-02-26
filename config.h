bf0b09c20e17248f4de31c76446e3eb0cb2713d8
Rob Morrison <mrrsn@live.com>
2018-02-26 01:03:37 -0500

    Slide the switch to go ever so slightly faster through the game

--- config.h
+++ config.h
@@ -9,6 +9,6 @@ const int _leds = 10; // 10 LEDs per level
 const int _levels = 10; // 10 levels per skill
 const int _maxSkill = 9; // 9 skills per game
 const int _skillWindow[] = { 700, 650, 600, 550, 550, 550, 500, 500, 500 }; // starting milliseconds per LED on level 1 of each skill
-const float _speedUpPct = 0.08; // percent of initial window to speed up for the current skill
+const float _speedUpPct = CircuitPlayground.slideSwitch() ? 0.08f : 0.09f; // percent of initial window to speed up for the current skill
 
 #endif
